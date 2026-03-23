f13fd3f82cbae5ca3c5e98cd68c0c5868c61d497
dung do <dtdung031004@gmail.com>
2026-03-23 18:35:32 +0700

    feat: implement basic audit logs api closes #15

--- Python/main.py
+++ Python/main.py
@@ -1,69 +1,31 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from datetime import datetime
+from typing import Optional
 
 app = FastAPI()
 
-users_db = []
+class AuditLog(BaseModel):
+    user_id: str
+    action: str
+    details: Optional[dict] = None
 
-class User(BaseModel):
-    id: int
-    name: str
-    email: str
+db = []
 
-@app.post("/api/users", status_code=201)
-def create_user(new_user: User):
-    users_db.append(new_user.model_dump())
+@app.get("/")
+def home():
+    return {"status": "Audit Module is running"}
 
-    return {
-        "statusCode": 201,
-        "success": True,
-        "message": "User created",
-        "data": new_user.model_dump()
+@app.post("/logs")
+def create_log(item: AuditLog):
+    new_entry = {
+        "id": len(db) + 1,
+        "timestamp": datetime.now(),
+        **item.dict()
     }
+    db.append(new_entry)
+    return new_entry
 
-@app.get("/api/users")
-def get_users():
-    return {
-        "statusCode": 200,
-        "success": True,
-        "message": "Get user list successfully",
-        "data": users_db 
-    }
-
-@app.put("/api/users/{user_id}")
-def update_user(user_id: int, updated_user: User):
-    for i, user in enumerate(users_db):
-        if user["id"] == user_id:
-            users_db[i] = updated_user.model_dump()
-            return {
-                "statusCode": 200,
-                "success": True,
-                "message": "User updated successfully",
-                "data": updated_user.model_dump()
-            }
-            
-    return {
-        "statusCode": 404,
-        "success": False,
-        "message": "User not found",
-        "data": {}
-    }
-
-@app.delete("/api/users/{user_id}")
-def delete_user(user_id: int):
-    for i, user in enumerate(users_db):
-        if user["id"] == user_id:
-            deleted_user = users_db.pop(i) 
-            return {
-                "statusCode": 200,
-                "success": True,
-                "message": "User deleted successfully",
-                "data": deleted_user
-            }
-
-    return {
-        "statusCode": 404,
-        "success": False,
-        "message": "User not found",
-        "data": {}
-    }
+@app.get("/logs")
+def get_logs():
+    return db
